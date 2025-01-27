8e2cbf6d132249c31f984c22d98ffdf969fb27b8
Riley Hutchinson <Rileyhutchinson@ymail.com>
2025-01-27 00:17:51 -0800

    Scatterplot -> HTML/CSS

--- scatterplot.js
+++ scatterplot.js
@@ -1,57 +1,44 @@
-function loadCSV(file){
-  Papa.parse(file, {
+fetch('resources/cleaned_data/all_trails_data_clean_df.csv')
+  .then(response => response.text())
+  .then(csv => Papa.parse(csv, {
+    header: true,
     complete: function(result) {
-      console.log(result.data);
-      createBubbleChart(result.data); 
-    },
-    header: true
-  })
-}
-
-document.getElementById('csvInput').addEventListener('change', function(e){
-  const file = e.target.files[0];
-  if (!file) {
-    alert("No file chosen!");  // Show a message if no file was selected
-  } else {
-    console.log(file);  // Log the file object to check if it's correctly selected
-    loadCSV(file);  // If a file is selected, parse and load it
-  }
-});
-
-function createBubbleChart(data){
-  // Prepare data for Plotly (scatter chart)
-  var trace1 = {
-    x: data.map(row => parseFloat(row.elevation)),  
-    y: data.map(row => parseFloat(row.distance)),  
-    mode: 'markers',  
-    type: 'scatter',
-    text: data.map(row => row.trail_name),  
-    hoverinfo: 'text', 
-    marker: {
-      color: data.map(row => parseFloat(row.elevation)),  
-      colorscale: 'Viridis',  
-      size: 6,  
-      colorbar: {  
-        title: 'Elevation',
-        
-      },
-      cmin: 10, 
-      cmax: 2000   
+      createBubbleChart(result.data);
     }
+  }));
+// Changed the code above this line to get the relative path of the csv file 
+
+function createBubbleChart(data) {
+  var trace1 = {
+      x: data.map(row => parseFloat(row['elevation_gain(feet)'])),  
+      y: data.map(row => parseFloat(row['distance(miles)'])),  
+      mode: 'markers',  
+      type: 'scatter',
+      text: data.map(row => row.trail_name),  
+      hoverinfo: 'text', 
+      marker: {
+          color: data.map(row => parseFloat(row['elevation_gain(feet)'])),  
+          colorscale: 'Viridis',  
+          size: 6,  
+          colorbar: {  
+              title: 'Elevation',
+          },
+          cmin: 10, 
+          cmax: 2000   
+      }
   };
 
   var layout = {
-    title: 'Hikes Elevations vs Distance',
-    xaxis: {
-      title: "Elevation",
-      range: [0,2000]  
-    },
-    yaxis: {
-      title: 'Distance',
-      range: [0, 17] 
-    }
+      title: 'Hikes Elevations vs Distance',
+      xaxis: {
+          title: "Elevation",
+          range: [0,2000]  
+      },
+      yaxis: {
+          title: 'Distance',
+          range: [0, 17] 
+      }
   };
 
-  // Use Plotly to create the chart inside the div with id "scatter-plot"
   Plotly.newPlot('scatter-plot', [trace1], layout);
 }
